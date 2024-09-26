89a4e2732dd93e91673c6f7dea38c51f0a4add8d
karlijones <karlijones1984@gmail.com>
2024-09-25 19:40:27 -0500

    Root

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import ReactDOM from "react-dom/client";
 function App() {
     return <h1>Hello React!</h1>
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
