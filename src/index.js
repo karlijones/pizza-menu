afb2c5f2867fc3f311845c9c9a1ed2a815ae63ad
karlijones <karlijones1984@gmail.com>
2024-09-26 20:55:00 -0500

    pizza spinaci image added

--- src/index.js
+++ src/index.js
@@ -59,6 +59,7 @@ function App() {
 function Pizza() {
     return (
     <div>
+        <img src="pizzas/spinaci.jpg" />
         <h2>Pizza Spinaci</h2>
         <p>
         Tomato, mozarella, spinach, and ricotta cheese
