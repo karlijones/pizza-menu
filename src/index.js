46e899fe87f0a28635440dde2343cdf9753af2c2
karlijones <karlijones1984@gmail.com>
2024-09-27 22:02:25 -0500

    isOpen Hours

--- src/index.js
+++ src/index.js
@@ -71,8 +71,18 @@ function Menu() {
 }
 
 function Footer() {
-    return <footer>{new Date().toLocaleTimeString()}We're currently open!</footer>
-};
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22
+    const isOpen = hour >= openHour && hour <= closeHour;
+
+    // if(hour >= openHour && <= closeHour) alert("We're currently open!"); 
+    // else alert("Sorry, we're closed")
+
+    return (
+    <footer>{new Date().toLocaleTimeString()}We're currently open!</footer>
+    );
+}
 
 
 function Pizza() {
