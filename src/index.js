3f1c945953120a9ec34c57841e8c761db1703734
karlijones <karlijones1984@gmail.com>
2024-09-28 21:15:24 -0500

    footer

--- src/index.js
+++ src/index.js
@@ -86,7 +86,9 @@ function Footer() {
     // else alert("Sorry, we're closed")
 
     return (
-    <footer>{new Date().toLocaleTimeString()}We're currently open!</footer>
+        <footer className="footer">
+            {new Date().toLocaleTimeString()}We're currently open!
+        </footer>
     );
 }
 
