455922e047090f9056f8fff5cecb5d97a9e8f939
karlijones <karlijones1984@gmail.com>
2024-09-28 21:16:22 -0500

    bug fix

--- src/index.js
+++ src/index.js
@@ -97,7 +97,7 @@ function Pizza() {
     return (
     <div>
         <img src="pizzas/spinaci.jpg" />
-        <h2>Pizza Spinaci</h2>
+        <h3>Pizza Spinaci</h3>
         <p>
         Tomato, mozarella, spinach, and ricotta cheese
         </p>
