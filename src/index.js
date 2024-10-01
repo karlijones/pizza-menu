5ded830434fd0293ea59ca5277942ac4e32eb62e
karlijones <karlijones1984@gmail.com>
2024-09-30 20:41:45 -0500

    added a space in footer after PM

--- src/index.js
+++ src/index.js
@@ -119,7 +119,7 @@ function Footer() {
 
     return (
         <footer className="footer">
-            {new Date().toLocaleTimeString()}We're currently open!
+            {new Date().toLocaleTimeString()} We're currently open!
         </footer>
     );
 }
