c094f3c5109dda6f704b76e7047e5a3bdbbfa6fb
karlijones <karlijones1984@gmail.com>
2024-09-30 20:37:12 -0500

    changed div to li

--- src/index.js
+++ src/index.js
@@ -97,14 +97,14 @@ function Menu() {
 
 function Pizza(props) {
     return (
-    <div className="pizza">
+    <li className="pizza">
         <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
         <div>
         <h3>{props.pizzaObj.name}</h3>
         <p>{props.pizzaObj.ingredients}</p>
         <span>{props.pizzaObj.price}</span>
         </div>
-    </div>
+    </li>
     );
 }
 
