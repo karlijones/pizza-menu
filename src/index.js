36ee9a9e4a9b01d1841c751ec5d92caa07632713
karlijones <karlijones1984@gmail.com>
2024-09-30 20:54:45 -0500

    added div with className and order button to footer

--- src/index.js
+++ src/index.js
@@ -119,7 +119,14 @@ function Footer() {
 
     return (
         <footer className="footer">
-            {isOpen && <p>We're open until {closeHour}:00. Come visit us or order online.</p>}
+            {isOpen && (
+                <div className="order">
+                <p>
+                We're open until {closeHour}:00. Come visit us or order online.
+                </p>
+                <button className="btn">Order</button>
+                </div>
+            )}
         </footer>
     );
 }
