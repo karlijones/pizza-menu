20ed503d87eac3f1dfe93e5bed2ccd1c8c9e4353
karlijones <karlijones1984@gmail.com>
2024-10-03 19:45:56 -0500

    destructiong props order and footer functions

--- src/index.js
+++ src/index.js
@@ -135,7 +135,7 @@ function Footer() {
     return (
         <footer className="footer">
             {isOpen ? (
-                <Order closeHour={closeHour} />
+                <Order closeHour={closeHour} openHour={openHour} />
             ) : (
             <p>
                     We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -146,10 +146,10 @@ function Footer() {
 }
 
 
-function Order(props) {
+function Order({closeHour, openHour}) {
     return <div className="order">
     <p>
-        We're open until {props.closeHour}:00. Come visit us or order online.
+        We're open until {closeHour}:00. Come visit us or order online.
     </p>
     <button className="btn">Order</button>
     </div>
