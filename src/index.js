753bc864dd85a6ac10dfdb13debc9dd2ac193264
karlijones <karlijones1984@gmail.com>
2024-10-04 20:50:07 -0500

    ternary operator to handle sold out

--- src/index.js
+++ src/index.js
@@ -120,7 +120,7 @@ function Pizza({pizzaObj}) {
         <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
         </div>
     </li>
     );
